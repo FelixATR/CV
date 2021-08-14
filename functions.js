095cac7a15b080b607ee0e70411057eb47811683
FelixATR <afrantz09@gmail.com>
2021-08-14 15:49:42 -0400

    initial populate
    adding files

---
+++ functions.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+    setInterval ('cursorAnimation()', 600);
+    type(document.getElementsByTagName("noscript")[0].innerHTML);
+});
+
+function type(text, new_caption_length) {
+    captionLength = new_caption_length || 0;
+
+    $('#caption').html(text.substr(0, captionLength++));
+    if(captionLength < text.length+1) {
+        setTimeout(function(){
+            type(text, captionLength);
+        }, 1);
+    }
+}
+
+function cursorAnimation() {
+    $('#cursor').animate({
+        opacity: 0
+    }, 'fast', 'swing').animate({
+        opacity: 1
+    }, 'fast', 'swing');
+}
+
